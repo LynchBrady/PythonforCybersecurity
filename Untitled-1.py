221002a30a4e76d32cb7cc115a1c1497967fdd5a
Brady Lynch <Lynch.brady@student.greenriver.edu>
2024-07-12 13:48:44 -0700

    Wrote a script that asks for a name and age and wrote an affirmation and new age based on the user inputs

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+#This asks for someone's name and saves it
+name = input("Hello! What is your name? ")
+
+#This prints the name given by the user
+print(f"Hello, {name}!")
+
+#This prints a positive affirmation
+print("Hi, have a good day today!")
+
+#This asks the user for their age
+age = int(input("How old are you? "))
+
+#This adds 2 to their age
+newAge = age + 2
+
+#This prints the new age
+print(f"In two years, you will be {newAge} years old!")
