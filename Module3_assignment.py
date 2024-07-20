8fe4dbe7109d6f33d1125ab0f50114c04bad10e9
Brady Lynch <Lynch.brady@student.greenriver.edu>
2024-07-20 16:45:16 -0700

    Made a program that asks the user how their day is going and responds to it.

---
+++ Module3_assignment.py
@@ -0,0 +1,8 @@
+#asks the user if today is a good day
+day = input("Is today a good day? y or n: ")
+
+#Responds to the user input
+if day == ("y"):
+    print("Yeah it is")
+if day == ("n"):
+    print("Then make it a good day")
---
+++ Module3_assignment1.py
@@ -0,0 +1,6 @@
+#asks the user if today is a good day
+day = input("Is today a good day? y or n: ")
+
+#Responds to the user input
+if day == ("y"):
+    print("Yeah it is")
