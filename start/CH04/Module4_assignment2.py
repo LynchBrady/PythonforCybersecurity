34c2f3e34178340c2d3a430678bf174ce3584413
Brady Lynch <Lynch.brady@student.greenriver.edu>
2024-07-27 15:07:16 -0700

    Did some stuff

---
+++ start/CH04/Module4_assignment2.py
@@ -0,0 +1,19 @@
+#Creates a file called "hackme.txt"
+hackme = open("hackme.txt", "w")
+
+#Asks for the user input
+NameQuestion = input("What is your name? ")
+ColorQuestion = input("What is your favorite color? ")
+PetQuestion = input("What was your first pet's name? ")
+MotherQuestion = input("What is your mother's maiden name? ")
+SchoolQuestion = input("What elementary school did you attend? ")
+
+#Saves code to "hackme.txt"
+hackme.write(f"{NameQuestion}\n")
+hackme.write(f"{ColorQuestion}\n")
+hackme.write(f"{PetQuestion}\n")
+hackme.write(f"{MotherQuestion}\n")
+hackme.write(f"{SchoolQuestion}\n")
+
+#Closes "hackme.txt"
+hackme.close()
